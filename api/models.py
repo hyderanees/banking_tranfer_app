338b055ee354b0f46f3f906111ce22e5e0c914c4
Haider Anees <haider.anees@thetowertech.com>
2022-04-22 04:45:05 +0500

    updated code

--- api/models.py
+++ api/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+from .utils import create_new_iban_number
 
 
 class Bank(models.Model):
@@ -6,4 +9,21 @@ class Bank(models.Model):
     address = models.TextField()
 
     def __str__(self):
-        return self.name + ' ' + self.address
+        return self.name + ' - ' + self.address
+
+    class Meta:
+        db_table = 'Bank'
+
+
+class Account(models.Model):
+    title = models.CharField(max_length=50, null=False, db_column='title')
+    IBAN = models.CharField(max_length=10, null=False, editable=False, unique=True, default=create_new_iban_number,
+                            db_column='IBAN')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, db_column='owner')
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE, db_column='bank')
+    balance = models.FloatField(null=False, default=0.0)
+    created_at = models.DateTimeField(auto_now_add=True, db_column='created_at')
+    updated_at = models.DateTimeField(auto_now=True, db_column='updated_at')
+
+    class Meta:
+        db_table = 'Account'
